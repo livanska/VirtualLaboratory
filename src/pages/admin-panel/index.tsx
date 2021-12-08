feedfb984a120116471f576a029cd75ead6482e5
Vladoseek <vlad.prishchepa@gmail.com>
2021-12-08 23:46:30 +0200

    fix admin panel

--- src/pages/admin-panel/index.tsx
+++ src/pages/admin-panel/index.tsx
@@ -7,102 +7,107 @@ import styles from "./styles.module.scss";
 
 const AdminPanel = () => {
   return (
-    <div className={styles.adminPanel}>
-      <GlassPanel className={styles.panel}>
-        <div className={styles.left}>
-          <ProfileInfo
-            fullName="Ivan Ivanov"
-            role="Student"
-            email="oleh@gmail.com"
-            onChangeName={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
+    <>
+      <div className={styles.background} />
+      <div className={styles.adminPanel}>
+        <GlassPanel className={styles.panel}>
+          <div className={styles.content}>
+            <div className={styles.left}>
+              <ProfileInfo
+                fullName="Ivan Ivanov"
+                role="Student"
+                email="oleh@gmail.com"
+                onChangeName={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
 
-          <div className={styles.form}>
-            <FieldGroup title="Module" />
+              <div className={styles.form}>
+                <FieldGroup title="Module" />
 
-            <FieldGroup title="Level" />
+                <FieldGroup title="Level" />
 
-            <FieldGroup
-              title="Task"
-              fieldProps={{ multiline: true, rows: 5 }}
-            />
+                <FieldGroup
+                  title="Task"
+                  fieldProps={{ multiline: true, rows: 5 }}
+                />
 
-            <div className={styles.buttons}>
-              <Button buttonstyle="secondary">Add Answer</Button>
-              <Button buttonstyle="primary">Save</Button>
+                <div className={styles.buttons}>
+                  <Button buttonstyle="secondary">Add Answer</Button>
+                  <Button buttonstyle="primary">Save</Button>
+                </div>
+              </div>
+            </div>
+
+            <div className={styles.right}>
+              <AdminTaskCard
+                title={"wadawwajhdawuhdwauihdwaiudhiawdhauidawhaiidawuhiawdiawhdiawddwadaw"}
+                description={"dwaadiawhdiuawhawdiuwahiudwahdiwuhdiuwahdiawuhdiawuhdwaihdaaaaaaaaaaaaawfawfawfdwadwa"}
+                maxAttempts={10}
+                maxTime={50}
+                onChangeAnswer={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+                onEdit={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+                onDelete={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
+
+              <AdminTaskCard
+                title={"wadawdwadaw"}
+                description={"dwaaaaaaaaaaaaaaawfawfawfdwadwa"}
+                maxAttempts={10}
+                maxTime={50}
+                onChangeAnswer={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+                onEdit={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+                onDelete={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
+
+              <AdminTaskCard
+                title={"wadawdwadaw"}
+                description={"dwaaaaaaaaaaaaaaawfawfawfdwadwa"}
+                maxAttempts={10}
+                maxTime={50}
+                onChangeAnswer={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+                onEdit={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+                onDelete={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
+
+              <AdminTaskCard
+                title={"wadawdwadaw"}
+                description={"dwaaaaaaaaaaaaaaawfawfawfdwadwa"}
+                maxAttempts={10}
+                maxTime={50}
+                onChangeAnswer={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+                onEdit={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+                onDelete={function (): void {
+                  throw new Error("Function not implemented.");
+                }}
+              />
             </div>
           </div>
-        </div>
-
-        <div className={styles.right}>
-          <AdminTaskCard
-            title={"wadawdwadaw"}
-            description={"dwaaaaaaaaaaaaaaawfawfawfdwadwa"}
-            maxAttempts={10}
-            maxTime={50}
-            onChangeAnswer={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            onEdit={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            onDelete={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-
-          <AdminTaskCard
-            title={"wadawdwadaw"}
-            description={"dwaaaaaaaaaaaaaaawfawfawfdwadwa"}
-            maxAttempts={10}
-            maxTime={50}
-            onChangeAnswer={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            onEdit={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            onDelete={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-
-          <AdminTaskCard
-            title={"wadawdwadaw"}
-            description={"dwaaaaaaaaaaaaaaawfawfawfdwadwa"}
-            maxAttempts={10}
-            maxTime={50}
-            onChangeAnswer={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            onEdit={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            onDelete={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-
-          <AdminTaskCard
-            title={"wadawdwadaw"}
-            description={"dwaaaaaaaaaaaaaaawfawfawfdwadwa"}
-            maxAttempts={10}
-            maxTime={50}
-            onChangeAnswer={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            onEdit={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            onDelete={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-        </div>
-      </GlassPanel>
-    </div>
+        </GlassPanel>
+      </div>
+    </>
   );
 };
 
