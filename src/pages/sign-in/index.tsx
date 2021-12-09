5afc17c520b4f2e700f1186c66b0f73ffca87ea8
Vladoseek <vlad.prishchepa@gmail.com>
2021-12-09 02:04:47 +0200

    add error handling in sign in and sign up

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -4,33 +4,47 @@ import figure from "../../assets/backgroundFigure.svg";
 import Button from "../../components/shared/Button";
 import FieldGroup from "../../components/shared/FieldGroup";
 import GlassPanel from "../../components/shared/GlassPanel";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 import styles from "./styles.module.scss";
 import { MainPageRoute, RegisterRoute } from "../../routes/routes";
 import { useState } from "react";
+import { Snackbar } from "@material-ui/core";
 
 const SignIn = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isError, setIsError] = useState(false);
+
   const logIn = () => {
     var data = {
-      "email": email,
-      "password": password
-    }
+      email: email,
+      password: password,
+    };
     var headers = {
       "Content-Type": "application/json",
-      'Access-Control-Allow-Origin': '*',
-    }
-    fetch('https://vlpz-backend.herokuapp.com/api/auth/signin', { method:'POST', headers: headers, body: JSON.stringify(data) })
+      "Access-Control-Allow-Origin": "*",
+    };
+    fetch("https://vlpz-backend.herokuapp.com/api/auth/signin", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    })
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-       localStorage.setItem('user', data.role)
-       window.location.href = MainPageRoute
+        if (!data.ok) throw Error();
+
+        localStorage.setItem("user", data.role);
+        window.location.href = MainPageRoute;
+      })
+      .catch((error) => {
+        setPassword("");
+        setIsError(true);
       });
-  }
+  };
+
   return (
     <>
       <img src={figure} className={styles.figure} />
@@ -44,15 +58,38 @@ const SignIn = () => {
 
           <div className={styles.signInForm}>
             <div className={styles.signInInputs}>
-              <FieldGroup title="Email" fieldProps={{ value: email, onChange: (e) => { setEmail(e.target.value) } }} />
-              <FieldGroup title="Password" fieldProps={{ value: password, onChange: (e) => { setPassword(e.target.value) }, type: 'password' }} />
+              <FieldGroup
+                title="Email"
+                fieldProps={{
+                  value: email,
+                  onChange: (e) => {
+                    setEmail(e.target.value);
+                  },
+                }}
+              />
+              <FieldGroup
+                title="Password"
+                fieldProps={{
+                  value: password,
+                  onChange: (e) => {
+                    setPassword(e.target.value);
+                  },
+                  type: "password",
+                }}
+              />
             </div>
 
             <p className={styles.registerText}>
               Don’t have an account yet?{" "}
-              <Link className={styles.registerLink} to={RegisterRoute}>Register now</Link>
+              <Link className={styles.registerLink} to={RegisterRoute}>
+                Register now
+              </Link>
             </p>
-            <Button buttonstyle="primary" className={styles.signInButton} onClick={logIn}>
+            <Button
+              buttonstyle="primary"
+              className={styles.signInButton}
+              onClick={logIn}
+            >
               Log In
             </Button>
           </div>
@@ -60,6 +97,13 @@ const SignIn = () => {
 
         <img src={logo} className={styles.logo} />
       </div>
+
+      <Snackbar
+        open={isError}
+        autoHideDuration={2000}
+        onClose={() => setIsError(false)}
+        message="Incorrect input data"
+      />
     </>
   );
 };
--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -9,34 +9,46 @@ import styles from "./styles.module.scss";
 import { Link } from "react-router-dom";
 import { LoginRoute, MainPageRoute } from "../../routes/routes";
 import { useState } from "react";
+import { Snackbar } from "@material-ui/core";
 
 const SignUp = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  const [lastName, setLastName] = useState('')
-  const [firstName, setFirstName] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [isError, setIsError] = useState(false);
+
   const Register = () => {
     var data = {
-      "email": email,
-      "password": password,
-      "confirmPassword":confirmPassword,
-      "name":firstName,
-      "surname": lastName
-    }
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+      name: firstName,
+      surname: lastName,
+    };
     var headers = {
       "Content-Type": "application/json",
-      'Access-Control-Allow-Origin': '*',
-    }
-    fetch('https://vlpz-backend.herokuapp.com/api/auth/signin', { method:'POST', headers: headers, body: JSON.stringify(data) })
+      "Access-Control-Allow-Origin": "*",
+    };
+    fetch("https://vlpz-backend.herokuapp.com/api/auth/signin", {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    })
       .then((response) => {
         return response.json();
       })
       .then((data) => {
-       localStorage.setItem('user', data.role)
-       window.location.href = MainPageRoute
+        if (!data.ok) throw Error();
+
+        localStorage.setItem("user", data.role);
+        window.location.href = MainPageRoute;
+      })
+      .catch((error) => {
+        setIsError(true);
       });
-  }
+  };
 
   return (
     <>
@@ -54,19 +66,65 @@ const SignUp = () => {
 
           <div className={styles.signUpForm}>
             <div className={styles.signUpInputs}>
-            <FieldGroup title="Email*" fieldProps={{ value: email, onChange: (e) => { setEmail(e.target.value) } }} />
-              <FieldGroup title="Password*" fieldProps={{ value: password, onChange: (e) => { setPassword(e.target.value) } }} />
-              <FieldGroup title="Confirm Password*" fieldProps={{ value: confirmPassword, onChange: (e) => { setConfirmPassword(e.target.value) } }}/>
-              <FieldGroup title="First Name" fieldProps={{ value: firstName, onChange: (e) => { setFirstName(e.target.value) } }} />
-              <FieldGroup title="Last Name" fieldProps={{ value: lastName, onChange: (e) => { setLastName(e.target.value) } }}/>
+              <FieldGroup
+                title="Email*"
+                fieldProps={{
+                  value: email,
+                  onChange: (e) => {
+                    setEmail(e.target.value);
+                  },
+                }}
+              />
+              <FieldGroup
+                title="Password*"
+                fieldProps={{
+                  value: password,
+                  onChange: (e) => {
+                    setPassword(e.target.value);
+                  },
+                }}
+              />
+              <FieldGroup
+                title="Confirm Password*"
+                fieldProps={{
+                  value: confirmPassword,
+                  onChange: (e) => {
+                    setConfirmPassword(e.target.value);
+                  },
+                }}
+              />
+              <FieldGroup
+                title="First Name"
+                fieldProps={{
+                  value: firstName,
+                  onChange: (e) => {
+                    setFirstName(e.target.value);
+                  },
+                }}
+              />
+              <FieldGroup
+                title="Last Name"
+                fieldProps={{
+                  value: lastName,
+                  onChange: (e) => {
+                    setLastName(e.target.value);
+                  },
+                }}
+              />
             </div>
 
             <p className={styles.loginText}>
               Already a member?{" "}
-              <Link className={styles.loginLink} to={LoginRoute}>Log in</Link>
+              <Link className={styles.loginLink} to={LoginRoute}>
+                Log in
+              </Link>
             </p>
 
-            <Button buttonstyle="primary" className={styles.signUpButton} onClick={Register}>
+            <Button
+              buttonstyle="primary"
+              className={styles.signUpButton}
+              onClick={Register}
+            >
               Register
             </Button>
           </div>
@@ -74,6 +132,13 @@ const SignUp = () => {
 
         <img src={logo} className={styles.logo} />
       </div>
+
+      <Snackbar
+        open={isError}
+        autoHideDuration={2000}
+        onClose={() => setIsError(false)}
+        message="An error has occured"
+      />
     </>
   );
 };
